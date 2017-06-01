15ca854b21d2020c3de5fc8ceda250edc3bd3a92
octaltree <octaltree@gmail.com>
2017-06-01 13:36:09 +0900

    akashisan.zsh split by function

--- home/.zsh/rc/host/akashisan.zsh
+++ home/.zsh/rc/host/akashisan.zsh
@@ -1,19 +1,9 @@
-if [ -z "$TMUX" ]; then
-  if [ `tmux list-sessions| sed '/attached/d'| wc -l` -ne 0 ]; then
-    target=`tmux list-sessions| sed '/attached/d'| awk -F: '{print $1}'| head -n 1`
-    tmux -2 attach-session -t $target
-  else
-    tmux -2
-  fi
-fi
-
-
 function haveCmd(){
   type $1 > /dev/null 2>&1
   return $?
 }
-proxy="proxy.uec.ac.jp:8080"
 function proxyFirefox(){
+  local proxy="$2"
   local prefs=`find ~/.mozilla/firefox -maxdepth 2 -name "prefs.js"`
   local on='"network.proxy.type", 1'
   local off='"network.proxy.type", 4'
@@ -21,13 +11,14 @@ function proxyFirefox(){
   echo "$prefs"| xargs -L1 sed $q -i
 }
 function proxyCargo(){
+  local proxy="$2"
   local on="proxy = \"$proxy\""
   local off="proxy = \"\""
   local q=`[ "$1" = "on" ] && echo "s/$off/$on/g" || echo "s/$on/$off/g"`
   sed $q -i ~/.cargo/config --follow-symlinks
 }
-if [ "\"UECWireless\"" = `iwconfig 2>/dev/null| sed '/ESSID/!d'| awk -F: '{print $2}'` ] ||\
-  [ "\"106F3F356510\"" = `iwconfig 2>/dev/null| sed '/ESSID/!d'| awk -F: '{print $2}'` ]; then
+function proxyOn(){
+  local proxy="$1"
   export http_proxy="$proxy"
   export https_proxy="$proxy"
   export HTTP_PROXY="$proxy"
@@ -41,9 +32,11 @@ if [ "\"UECWireless\"" = `iwconfig 2>/dev/null| sed '/ESSID/!d'| awk -F: '{print
   echo "proxy=$proxy" >> ~/.curlrc
   # /systemd/system/multi-user.target.wants/docker.service
   # Environment="HTTP_PROXY=http://proxy.uec.ac.jp:8080/,HTTPS_PROXY=http://proxy.uec.ac.jp:8080/"
-  proxyFirefox on
-  proxyCargo on
-else
+  proxyFirefox on $proxy
+  proxyCargo on $proxy
+}
+function proxyOff(){
+  local proxy="$1"
   export http_proxy=""
   export https_proxy=""
   export HTTP_PROXY=""
@@ -56,6 +49,25 @@ else
     python3 -c "fr = open('$HOME/.ssh/config', 'r'); str=fr.read().replace('Host *\n  ProxyCommand nc -X connect -x $proxy %h %p\n', ''); fw = open('$HOME/.ssh/config', 'w'); fw.write(str)"
     python3 -c "fr = open('$HOME/.curlrc', 'r'); str=fr.read().replace('proxy=$proxy\n', ''); fw = open('$HOME/.curlrc', 'w'); fw.write(str)"
   fi
-  proxyFirefox off
-  proxyCargo off
-fi
+  proxyFirefox off $proxy
+  proxyCargo off $proxy
+}
+
+(){
+  if [ -z "$TMUX" ]; then
+    if [ `tmux list-sessions| sed '/attached/d'| wc -l` -ne 0 ]; then
+      target=`tmux list-sessions| sed '/attached/d'| awk -F: '{print $1}'| head -n 1`
+      tmux -2 attach-session -t $target
+    else
+      tmux -2
+    fi
+  fi
+
+  local proxy="proxy.uec.ac.jp:8080"
+  if [ "\"UECWireless\"" = `iwconfig 2>/dev/null| sed '/ESSID/!d'| awk -F: '{print $2}'` ] ||\
+    [ "\"106F3F356510\"" = `iwconfig 2>/dev/null| sed '/ESSID/!d'| awk -F: '{print $2}'` ]; then
+    proxyOn $proxy
+  else
+    proxyOff $proxy
+  fi
+}
