d8d04b681f0da39b9e671e33765ab97bf67cd91b
octaltree <octaltree@gmail.com>
2017-06-12 16:05:59 +0900

    Host *.*

--- home/.zsh/rc/host/akashisan.zsh
+++ home/.zsh/rc/host/akashisan.zsh
@@ -27,8 +27,7 @@ function proxyOn(){
     git config --global http.proxy $proxy
     git config --global https.proxy $proxy
   fi
-  echo "Host *" >> ~/.ssh/config
-  echo "  ProxyCommand nc -X connect -x $proxy %h %p" >> ~/.ssh/config
+  echo "Host *.*\n  ProxyCommand nc -X connect -x $proxy %h %p" >> ~/.ssh/config
   echo "proxy=$proxy" >> ~/.curlrc
   # /systemd/system/multi-user.target.wants/docker.service
   # Environment="HTTP_PROXY=http://proxy.uec.ac.jp:8080/,HTTPS_PROXY=http://proxy.uec.ac.jp:8080/"
@@ -45,10 +44,11 @@ function proxyOff(){
     git config --global http.proxy ''
     git config --global https.proxy ''
   fi
-  if haveCmd "python3"; then
-    python3 -c "fr = open('$HOME/.ssh/config', 'r'); str=fr.read().replace('Host *\n  ProxyCommand nc -X connect -x $proxy %h %p\n', ''); fw = open('$HOME/.ssh/config', 'w'); fw.write(str)"
-    python3 -c "fr = open('$HOME/.curlrc', 'r'); str=fr.read().replace('proxy=$proxy\n', ''); fw = open('$HOME/.curlrc', 'w'); fw.write(str)"
-  fi
+  # TODO
+  #if haveCmd "python3"; then
+  #  python3 -c "fr = open('$HOME/.ssh/config', 'r'); print(fr.read()); str=fr.read().replace('Host *.*\n  ProxyCommand nc -X connect -x $proxy %h %p\n', ''); print(str); fw = open('$HOME/.ssh/config', 'w'); fw.write(str)"
+  #fi
+  sed '/^proxy=.*$/d' -i ~/.curlrc
   proxyFirefox off $proxy
   proxyCargo off $proxy
 }
