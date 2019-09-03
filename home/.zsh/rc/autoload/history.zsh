82c4b110012b3d78724de66b73c949dd8ed4d631
octaltree <octaltree@gmail.com>
2019-09-03 18:17:59 +0900

    fix

--- home/.zsh/rc/autoload/history.zsh
+++ home/.zsh/rc/autoload/history.zsh
@@ -10,6 +10,6 @@ setopt share_history        # share command history data
 #historyをいい感じに#############################
 autoload history-search-end
 zle -N history-beginning-search-backward-end history-search-end
-zle -N history-beginning-search-forward-end hisotry-search-end
+zle -N history-beginning-search-forward-end history-search-end
 bindkey -a "k" history-beginning-search-backward-end
-bindkey -a "^N" hisotry-beginning-search-forward-end
+bindkey -a "^N" history-beginning-search-forward-end
