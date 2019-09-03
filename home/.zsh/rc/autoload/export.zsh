08d9b27ee4dd045feeaf0af540cae5965e8c3932
octaltree <octaltree@gmail.com>
2019-09-03 18:53:05 +0900

    rm recExport

--- home/.zsh/rc/autoload/export.zsh
+++ home/.zsh/rc/autoload/export.zsh
@@ -13,25 +13,27 @@ export GOPATH="$HOME/.go"
 which nvim > /dev/null 2>&1 &&\
   export EDITOR=nvim
 
-function recExport(){
-  # シンボリックリンクであればリンク先も再帰的に
-  # 無限ループに気をつけて
-  if [ ! -d $1 ]; then return 1; fi
-  dir=`cd $1 > /dev/null; pwd`
-  if [ `echo $dir| awk -F/ '{print $NF}'` = ".git" ]; then return 0; fi
-  export PATH="$dir:${PATH}"
-  for i in `find "$dir" -maxdepth 1 -mindepth 1 -type d -o -type l`; do
-    if [ -L $i ]; then
-      recExport `readlink $i`
-    else
-      recExport $i
-    fi
-  done
-}
-
-if [ -d "${HOME}/bin" ]; then
-  recExport "${HOME}/bin"
-fi
+#function recExport(){
+#  # シンボリックリンクであればリンク先も再帰的に
+#  # 無限ループに気をつけて
+#  if [ ! -d $1 ]; then return 1; fi
+#  dir=`cd $1 > /dev/null; pwd`
+#  if [ `echo $dir| awk -F/ '{print $NF}'` = ".git" ]; then return 0; fi
+#  export PATH="$dir:${PATH}"
+#  for i in `find "$dir" -maxdepth 1 -mindepth 1 -type d -o -type l`; do
+#    if [ -L $i ]; then
+#      recExport `readlink $i`
+#    else
+#      recExport $i
+#    fi
+#  done
+#}
+#
+#if [ -d "${HOME}/bin" ]; then
+#  recExport "${HOME}/bin"
+#  echo "$PATH" > /tmp/cache-zsh-path
+#fi
+export PATH="${HOME}/bin/handyScript:${HOME}/bin:${PATH}"
 
 function removePath(){
   export PATH=`echo -n $PATH | awk -v RS=: -v ORS=: '$0 != "'$1'"' | sed 's/:$//'`;
