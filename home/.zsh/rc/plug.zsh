243f20a8b0f23c30c094a0cf46874877462cdd3c
octaltree <octaltree@gmail.com>
2019-11-02 13:09:38 +0900

    disable incr

--- home/.zsh/rc/plug.zsh
+++ home/.zsh/rc/plug.zsh
@@ -2,7 +2,7 @@ source ~/.zsh/plugins/zplugin/zplugin.zsh
 autoload -Uz _zplugin
 (( ${+_comps} )) && _comps[zplugin]=_zplugin
 
-zplugin ice wait'' atinit'zpcompinit'; zplugin load _local/incr
+#zplugin ice wait'' atinit'zpcompinit'; zplugin load _local/incr
 zplugin ice wait'' atinit'zpcompinit'; zplugin load _local/auto_menu
 zplugin ice wait'!0'; zplugin load _local/sandbox
 zplugin ice wait'!0'; zplugin load zsh-users/zsh-syntax-highlighting
