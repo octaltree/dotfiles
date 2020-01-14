dd7b150d5eb1ab5ec35abbe7e46ce80832919545
octaltree <octaltree@gmail.com>
2020-01-14 16:03:37 +0900

    keybind for zsh-autosuggest

--- home/.zsh/rc/plug.zsh
+++ home/.zsh/rc/plug.zsh
@@ -13,4 +13,7 @@ zplugin ice wait'!0'; zplugin load zsh-users/zsh-completions
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=5"
 ZSH_AUTOSUGGEST_STRATEGY=(completion history)
 ZSH_AUTOSUGGEST_USE_ASYNC=1
+ZSH_AUTOSUGGEST_ACCEPT_WIDGETS="forward-word,vi-forward-blank-word-end"
+ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS=""
 bindkey -M viins '^f' forward-word
+bindkey -M vicmd '^f' vi-forward-blank-word-end
