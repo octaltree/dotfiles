de513f8802ca50d9cd58b477dff6ef5617f08520
octaltree <octaltree@gmail.com>
2020-01-19 11:04:54 +0900

    fzf

--- home/.zplugin/plugins/_local---auto_menu/main.zsh
+++ home/.zplugin/plugins/_local---auto_menu/main.zsh
@@ -1,1 +1,9 @@
 setopt auto_menu # 補完キー連打で順に補完候補を自動で補完
+
+function select-history(){
+  BUFFER=$(history -n -r 1 | fzf --no-sort +m --query "$BUFFER")
+  CURSOR=$#BUFFER
+}
+
+zle -N select-history
+bindkey -M vicmd 'H' select-history
