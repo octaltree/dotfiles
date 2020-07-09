2877c635627495229f0d0173bf72f5215dd163e3
octaltree <octaltree@gmail.com>
2020-07-09 13:12:48 +0900

    deploy

--- subcmd/deploy.sh
+++ subcmd/deploy.sh
@@ -2,20 +2,17 @@
 
 function copyDirSymRec(){
   # ディレクトリは作成 ファイルはシンボリックリンク を再帰的に
-  # ただしhostというディレクトリがあったら中の `hostname -s`に該当するファイルに置き換える
+  # ただしhosts/はhosts/`hostname -s`/*についてhostと同じ階層にリンクを張る
   local originfiles=($(ls -ap $1| sed '/\//d'))
   local origindirs=($(ls -ap $1| sed '/\//!d'| awk -F/ '{print $1}'| sed '/^\.$/d' | sed '/^\.\.$/d'))
   for i in ${originfiles[@]}; do
     _failsafe _ newSym $1/$i $2/$i
   done
   for i in ${origindirs[@]}; do
-    if [ $i = "host" ]; then
-      if [ $(ls -a1 $1/host| grep `hostname -s`| wc -l) = "1" ]; then
-        filename=`ls -a1 $1/host| grep $(hostname -s)`
-        from="$1/host/$filename"
-        to="$2/$filename"
-        _failsafe _ newSym $from $to
-      fi
+    if [ $i = "hosts" ]; then
+      h="$1/hosts/`hostname -s`"
+      if ! [ -d "$h" ]; then continue; fi
+      copyDirSymRec $h $2
     else
       _failsafe _ newDir $2/$i
       copyDirSymRec $1/$i $2/$i
