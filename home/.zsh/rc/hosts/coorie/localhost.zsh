8176f626769de236d6fcf969b1e0eac300d822e5
octaltree <octaltree@gmail.com>
2020-07-10 17:40:28 +0900

    tmux

---
+++ home/.zsh/rc/hosts/coorie/localhost.zsh
@@ -0,0 +1,11 @@
+
+(){
+  if [ -z "$TMUX" ]; then
+    if [ `tmux list-sessions| sed '/attached/d'| wc -l` -ne 0 ]; then
+      target=`tmux list-sessions| sed '/attached/d'| awk -F: '{print $1}'| head -n 1`
+      tmux attach-session -t $target
+    else
+      tmux
+    fi
+  fi
+}
