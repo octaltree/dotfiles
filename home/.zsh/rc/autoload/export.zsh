5db71b8df39bc3b5b9dfe5b72c9b560c8ce6f43d
octaltree <octaltree@users.noreply.github.com>
2021-04-06 16:20:13 +0900

    JAVA_HOME

--- home/.zsh/rc/autoload/export.zsh
+++ home/.zsh/rc/autoload/export.zsh
@@ -2,10 +2,10 @@ which nvim > /dev/null 2>&1 &&\
   export EDITOR=nvim
 
 typeset -U PATH
-export PATH="$PATH:$HOME/.local/bin"
 export PATH="$PATH:$HOME/.cargo/bin"
 export PATH="$HOME/bin/handyScript:$PATH"
 export PATH="$HOME/bin:$PATH"
 
 export RUST_BACKTRACE=1
 export GOPATH="$HOME/.go"
+export JAVA_HOME=/opt/android-studio/jre
