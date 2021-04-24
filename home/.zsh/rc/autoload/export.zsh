7e479f7dea8e41182d2b363a2f2508967a5ea369
octaltree <octaltree@users.noreply.github.com>
2021-04-24 23:57:08 +0900

    set RUSTC_WRAPPER sccache

--- home/.zsh/rc/autoload/export.zsh
+++ home/.zsh/rc/autoload/export.zsh
@@ -7,5 +7,7 @@ export PATH="$HOME/bin/handyScript:$PATH"
 export PATH="$HOME/bin:$PATH"
 
 export RUST_BACKTRACE=1
+which sccache > /dev/null 2>&1 &&\
+  export RUSTC_WRAPPER=`which sccache`
 export GOPATH="$HOME/.go"
 export JAVA_HOME=/opt/android-studio/jre
