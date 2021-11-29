268e32aca9cf6130669417c6931df84a490aad01
octaltree <octaltree@users.noreply.github.com>
2021-11-29 21:31:17 +0900

    clap

--- home/.config/nvim/lua/plugafter.lua
+++ home/.config/nvim/lua/plugafter.lua
@@ -10,7 +10,8 @@ do
     }
     linearf.recipe.matchers = {
         {name = "identity", path = "flavors_plain::Identity"},
-        {name = "substring", path = "flavors_plain::Substring"}
+        {name = "substring", path = "flavors_plain::Substring"},
+        {name = "clap", path = "flavors_clap::Clap"}
     }
     linearf.recipe.converters = {
         {name = "format_line", path = "flavors_plain::FormatLine"}
