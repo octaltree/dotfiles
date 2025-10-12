9846e48e3b18c0af8dcd8928adfd7e835b743f44
octaltree <octaltree@users.noreploy.github.com>
2025-10-12 18:44:26 +0900

    nao

---
+++ home/.zsh/rc/hosts/nao/localhost.zsh
@@ -0,0 +1,13 @@
+
+(){
+  if [ -z "$TMUX" ]; then
+    if [ `tmux list-sessions| sed '/attached/d'| wc -l` -ne 0 ]; then
+      target=`tmux list-sessions| sed '/attached/d'| awk -F: '{print $1}'| head -n 1`
+      tmux attach-session -t $target
+    else
+      tmux
+    fi
+  fi
+
+  #source /usr/share/nvm/init-nvm.sh
+}
